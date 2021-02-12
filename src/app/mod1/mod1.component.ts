61fde988c8d0acf291ede8a020bb61ed49d10095
garlife <garlife@gmail.com>
2021-02-13 01:35:53 +0300

    end 1 lect video

--- src/app/mod1/mod1.component.ts
+++ src/app/mod1/mod1.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-mod1',
   templateUrl: './mod1.component.html',
-  styleUrls: ['./mod1.component.css']
+  styleUrls: ['./mod1.component.css'],
 })
-export class Mod1Component implements OnInit {
-
-  constructor() { }
-
+export class Mod1Component
+  implements OnChanges, 
+  OnInit, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+  {
+  logs: string[] = [];
+  name:string = '';
+  showChild:boolean = true;
+  constructor() {
+    this.name = "Lena";
+    setTimeout(() => {this.updateName()}, 5000); //сигнатура стрелочной функции
+    setTimeout(() => {this.hideChild()}, 10000)
+  }
+  ngAfterContentChecked(): void {
+     console.log('app-mod1', 'ngAfterContentChecked', this.logs);
+  }
+  ngAfterViewInit(): void {
+     console.log('app-mod1', 'ngAfterViewInit', this.logs);
+  }
+  ngAfterViewChecked(): void {
+     console.log('app-mod1', 'ngAfterViewChecked', this.logs);
+  }
+  ngOnDestroy(): void {
+     console.log('app-mod1', 'ngOnDestroy', this.logs);
+  }
+  ngAfterContentInit(): void {
+     console.log('app-mod1', 'ngAfterContentInit', this.logs);
+  }
+  ngDoCheck(): void {
+     console.log('app-mod1', 'ngDoCheck', this.logs);
+  }
   ngOnInit(): void {
+    console.log('app-mod1', 'ngOnInit', this.logs);
+  }
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log('app-mod1', 'ngOnChanges', this.logs, changes);
   }
 
+  onLog(data:string): void {
+    this.logs.push(data);
+  }
+
+  updateName(): void {
+    this.name = "Sasha";
+  }
+
+  hideChild(): void {
+    this.showChild = false;
+    this.onLog("onDestroy");
+    
+  }
+  // ngOnInit(): void {
+  // }
 }
--- src/app/mod1/mod1.module.ts
+++ src/app/mod1/mod1.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Mod1Component } from './mod1.component';
-
+import { ChildComponent } from './child/child.component';
 
 
 @NgModule({
-  declarations: [Mod1Component],
+  declarations: [Mod1Component, ChildComponent],
   imports: [
     CommonModule
   ],
